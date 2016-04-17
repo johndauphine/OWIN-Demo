cf8d2a442dd1e36f366d37b756231c96025d1db2
John Dauphine <johndauphine@hotmail.com>
2016-04-16 19:17:52 -0500

    Init Commit

---
+++ OwinDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwinDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello OWIN");
+            Console.ReadLine();
+        }
+    }
+}
