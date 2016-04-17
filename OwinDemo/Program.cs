4cbd30e5c1fff7730ad089d53c89a7fb4358c250
John Dauphine <johndauphine@hotmail.com>
2016-04-16 19:40:25 -0500

    Now the app actually hosts a web service

--- OwinDemo/Program.cs
+++ OwinDemo/Program.cs
@@ -1,16 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Net.Http;
 
 namespace OwinDemo
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello OWIN");
+            string baseAddress = "http://localhost:9000/";
+
+            // Start OWIN host 
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+                // Create HttpCient and make a request to api/values 
+                HttpClient client = new HttpClient();
+
+                var response = client.GetAsync(baseAddress + "api/values").Result;
+
+                Console.WriteLine(response);
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+            }
+
             Console.ReadLine();
         }
     }
